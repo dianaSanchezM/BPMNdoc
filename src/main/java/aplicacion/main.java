cc3c252cd9743c2ada978d47cd5a1a9444949523
DianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2018-02-18 13:57:53 -0500

    implementacion main

--- src/main/java/aplicacion/main.java
+++ src/main/java/aplicacion/main.java
@@ -5,14 +5,17 @@
  */
 package aplicacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author diana
  */
 public class main {
     public static void main(String[] args) throws Exception {
-        String filename = "Solicitud - 1.0.bpmn";
-
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Digite el nombre del archivo bpmn");
+        String filename = entrada.nextLine();
         BpmnDoc.leerBPMN2(filename);
         BpmnDoc.crearArchivoHtml(filename);
         
